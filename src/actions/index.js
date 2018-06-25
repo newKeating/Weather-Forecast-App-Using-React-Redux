5a6de43213bcd511f8687d9c30b5129aa80adac6
Peter Kim <kroits09@gmail.com>
2018-06-25 14:07:18 +0900

    Change the API endpoint

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,12 @@
 import axios from 'axios';
 import API_KEY from '../api_key';
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export const fetchWeather = (city) => {
   const url = `${ROOT_URL}&q=${city},us`;
   const request = axios.get(url);
-
-  console.log('Request:', request);
 
   return {
     type: FETCH_WEATHER,
